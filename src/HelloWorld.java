f1668a100d3a7fb62949053397cbea36e13b74de
USER <USER@DESKTOP-1F88JRD>
2020-11-15 21:42:11 +0900

    index

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld_review");
+		System.out.println("Hello, I'm developer!");
+		
+	}
+
+}
